25fe0cff1ddadc55eb89c39019da825186bab580
ploukareas <ploukareas@gmail.com>
2022-09-16 09:12:29 +0300

    add flir models from SDK

---
+++ Models/FLIRImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flir.Atlas.Image;
+
+namespace WpfAppFLIRAtlas.Models
+{
+	
+	public class FLIRImageModel
+    {
+        public ThermalImage ImageThermal {get; set; }
+    }
+}
